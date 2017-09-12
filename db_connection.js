c169895e8b2610a259a3202f4e97c3e15c9ef235
AlfredMothapo <mthmas016@myuct.ac.za>
2017-09-12 21:49:55 +0200

    Fixed db_connection class

--- db_connection.js
+++ db_connection.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 
-export class DBcon{
-  constructor(){
+export class DBcon {
+  constructor() {
     this.con = mysql.createConnection({
         host: 'localhost',
         user: 'root', //change to your username
@@ -9,14 +9,14 @@ export class DBcon{
         database: 'cb'  //your database name
     });
   }
-  getConnection(){  //connects to mysql and returns the connection
-    this.con.connect( (err) => {
+  getConnection() {  //connects to mysql and returns the connection
+    this.con.connect((err) => {
       if (err) throw err;
-      console.log('Database connected!')
-    })
+      console.log('Database connected!');
+    });
     return this.con;
   }
-  close_connection(){  //closes the connection
+  closeConnection() {  //closes the connection
     this.con.end();
   }
 }
