dbb47f3f3349ef6956ff31966d44c41c4d876cb9
Alfred Mothapo <mthmas016@myuct.ac.za>
2017-10-03 17:41:50 +0200

    Login fixed!

--- models/login.js
+++ models/login.js
@@ -11,25 +11,29 @@ export class LoginModel {
   static getUser(username, password, resp) {
     const sql2 = 'SELECT password FROM users WHERE email = ?';
     connection.query(sql2, [username], (err, fields) => {
-          const pwd = fields[0].password;
-          if (err) {
-                throw err;
-              } else if (bcrypt.compareSync(password.toString(), pwd.toString()) === true) {
-                //log them in
-                const sql = 'select first_name,email,users.user_id,access_id,node_id' +
-                ' from users ' +
-                'INNER JOIN membership ON users.user_id = membership.user_id ' +
-                'WHERE users.email=? AND users.password=?  ';
-                return new Promise((resolve, reject) => {
-                  connection.query(sql, [username, pwd], (error, fieldsa) => {
-                      if (error) return reject(error);
-                      resolve(fieldsa);
-                      resp.end(stringify(fieldsa, null, 1));
+          try {
+            const pwd = fields[0].password;
+            if (err) {
+                  throw err;
+                } else if (bcrypt.compareSync(password.toString(), pwd.toString()) === true) {
+                  //log them in
+                  const sql = 'select first_name,email,users.user_id,access_id,node_id' +
+                  ' from users ' +
+                  'INNER JOIN membership ON users.user_id = membership.user_id ' +
+                  'WHERE users.email=? AND users.password=?  ';
+                  return new Promise((resolve, reject) => {
+                    connection.query(sql, [username, pwd], (error, fieldsa) => {
+                        if (error) return reject(error);
+                        resolve(fieldsa);
+                        resp.end(stringify(fieldsa, null, 1));
+                    });
                   });
-                });
-              } else {
-                resp.end('Wrong login details');
-              }
+                } else {
+                  resp.end('Wrong login details');
+                }
+          } catch (e) {
+            resp.end('Wrong login details');
+          }
         });
   }
 }
