c0e9e586ea8ac943ec498bd2b552eaffafe00abd
Emilie Wood <emilie@anotherway.co.za>
2017-10-04 16:56:15 +0200

    fix publication type

--- models/publications.js
+++ models/publications.js
@@ -5,22 +5,24 @@ const connection = con.getConnection();
 export class PublicationModel {
   static getPublications() {
     const sql = 'select type_id,type from research_types';
-    return new Promise((reject, resolve) => {
+    return new Promise((resolve, reject) => {
       connection.query(sql, (err, fields) => {
-        if (err) reject(err);
+        if (err) {
+          return reject(err);
+        } 
         resolve(fields);
       });
     });
   }
 
   static getPublicationsById(publicationId) {
-    const sql = 'SELECT type_id, type FROM research_type';
-    return new Promise((reject, resolve) => {
-      connection.query(sql, (err, fields) => {
+    const sql = 'SELECT type_id, type FROM research_types WHERE type_id=?';
+    return new Promise((resolve, reject) => {
+      connection.query(sql, [publicationId], (err, fields) => {
         if (err) {
-          reject(err);
+          return reject(err);
         }
-        resolve(fields);
+        resolve(fields[0]);
       });
     });
   }
